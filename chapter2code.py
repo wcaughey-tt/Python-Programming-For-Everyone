bf8b4f005dd93c236d9c0baac24462efc35f8d2a
wcaughey-tt <wcaughey@thumbtack.com>
2019-12-24 17:20:32 -0700

    Create chapter2code.py

---
+++ chapter2code.py
@@ -0,0 +1,21 @@
+"""
+
+    Title:      Chapter 2 Code
+    Purpose:    (Knowledge Development) Learning Python in Coursera's Python for Everyone Specialization
+    Author:     Billy Caughey
+    Date:       2019.12.24 - Initial Build
+
+"""
+
+##### Exercise 2.2 #####
+
+# The code below almost works
+
+name = input("Enter your name")
+print("Hello " + name)
+
+##### Exercise 2.3 #####
+
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter rate per hour:"))
+print(hrs * rate)
